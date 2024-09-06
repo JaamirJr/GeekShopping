548b76e33b96ea8013ba2eaf2d59cd83c9b4b598
Jamir Junior <jamir.junior@hotmail.com>
2024-09-05 22:30:49 -0300

    Commit inicial

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+	public class MySqlContext : DbContext
+	{
+        public MySqlContext() {}
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base (options) { }
+
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(new Product 
+			{ 
+				Id = 2,
+				Name = "Name",
+				Price = 69.9M,
+				Description = "Description",
+				CategoryName = "CategoryName",
+				ImageUrl = null
+			});
+
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,47 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration["MySqlConnection:MySqlConnectString"];
+
+builder.Services.AddDbContext<MySqlContext>(options =>
+	options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+//builder.Services.AddSwaggerGen(c =>
+//{
+//	c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+//});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
